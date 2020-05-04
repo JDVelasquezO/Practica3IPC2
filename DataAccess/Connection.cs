f838f598ec500216bab7778a9c9ba3c98adbc703
JDVelasquezO <jdvela158@gmail.com>
2020-05-04 12:42:31 -0600

    Beigining with the project

---
+++ DataAccess/Connection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Connection
+    {
+        string stringConn = "Data Source=localhost;Initial Catalog=practica1;Integrated Security=True";
+        public SqlConnection conn = new SqlConnection();
+
+        public Connection()
+        {
+            conn.ConnectionString = stringConn;
+        }
+
+        public void open()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void close()
+        {
+            conn.Close();
+        }
+
+        public SqlConnection returnConn()
+        {
+            return conn;
+        }
+    }
+}
